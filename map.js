a11ac541a74e7a61735d53b84989eeed5df168a0
Samson Poon <samsonkfpoon@gmail.com>
2023-01-13 03:04:27 +0000

    callback with map function

---
+++ map.js
@@ -0,0 +1,35 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    } 
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  } 
+};  
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = (array, callback) => {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results; 
+};
+
+const results1 = map(words, word => word[0]);
+console.log("results1", results1)
+
+console.log(assertArraysEqual(results1, ["g", "c", "t", "m", "t"]))
+console.log(assertArraysEqual(results1, ["g", "c", "t", "m", "u"]))
